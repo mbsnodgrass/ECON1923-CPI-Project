424d94e0f9e48b2bb11a96d111307fdda7431e89
Maxwell Snodgrass <72468812+mbsnodgrass@users.noreply.github.com>
2022-10-10 17:23:00 -0400

    Add files via upload

---
+++ CPIPart1.py
@@ -0,0 +1,33 @@
+8 Items Each for 3 Locations
+
+*CollegeTextbooks* + *BedroomFurniture* + *CollegeTuition* + *Computers* + *Gas* + *Rent* + *Coffee* + *Laundry Services*
+
+Pittsburgh
+* College Textbooks: CUURA104SSEA011
+* Bedroom Furniture: CUURA104SEHJ01
+* College Tuition/Fees: CUURA104SEEB01
+* Computers: CUURA104SEEE01
+* Gas: CUURA104SS47014
+* Rent of Shelter: CUURA104SAS2RS
+* Coffee: CUURA104SEFP01
+* Laundry Services: CUURA104SEGD03
+
+Cleveland/Akron
+* College Textbooks: CUURA210SSEA011
+* Bedroom Furniture: CUURA210SEHJ01
+* College Tuition/Fees: CUURA210SEEB01
+* Computers: CUURA210SEEE01
+* Gas: CUURA210SS47014
+* Rent of Shelter: CUURA210SAS2RS
+* Coffee: CUURA210SEFP01
+* Laundry Services: CUURA210SEGD03
+
+Baltimore/Washington
+* College Textbooks: CUURA311SSEA011
+* Bedroom Furniture: CUURA311SEHJ01
+* College Tuition/Fees: CUURA311SEEB01
+* Computers: CUURA311SEEE01
+* Gas: CUURA311SS47014
+* Rent of Shelter: CUURA311SAS2RS
+* Coffee: CUURA311SEFP01
+* Laundry Services: CUURA311SEGD03
