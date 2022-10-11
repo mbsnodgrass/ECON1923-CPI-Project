f4fb8041169ae6c20cd336e3bbcbd02be2c9d3b9
Maxwell Snodgrass <72468812+mbsnodgrass@users.noreply.github.com>
2022-10-11 11:34:29 -0400

    FixedPart1

--- CPIPart1.py
+++ CPIPart1.py
@@ -1,35 +1,28 @@
-8 Items Each for 3 Locations
+New England (Location 1: 
+* P1 Food: CUUR0110SAF1
+* P2 Gas: CUUR0110SS47014
+* P3 Rent of Shelter: CUUR0110SAS2RS
+* P4 Apparrel: CUUR0110SAA
 
-.2(*CollegeTextbooks*) + .05(*BedroomFurniture*) + .2(*CollegeTuition*) + .2(*Computers*) + .05(*Gas*) + .15(*Rent*) + .05(*Coffee*) + .1(*Laundry Services*)
+Middle Atlantic (Location 2:
+* P1 Food: CUUR0120SAF1
+* P2 Gas: CUUR0120SS47014
+* P3 Rent of Shelter: CUUR0120SAS2RS
+* P5 Educational Books/Supplies: CUUR0120SEEA
+* P6 Food Away From Home: CUUR0120SEFV
 
-Pittsburgh
-* College Textbooks: CUURA104SSEA011
-* Bedroom Furniture: CUURA104SEHJ01
-* College Tuition/Fees: CUURA104SEEB01
-* Computers: CUURA104SEEE01
-* Gas: CUURA104SS47014
-* Rent of Shelter: CUURA104SAS2RS
-* Coffee: CUURA104SEFP01
-* Laundry Services: CUURA104SEGD03
+South Alantic (Location 3: 
+* P1 Food: CUUR0350SAF1
+* P2 Gas: CUUR0350SS47014
+* P3 Rent of Shelter: CUUR0350SAS2RS
+* P7 Personal Care Services: CUUR0350SEGC
+* P8 Transportation Services: CUUR0350SAS4
 
-Cleveland/Akron
-* College Textbooks: CUURA210SSEA011
-* Bedroom Furniture: CUURA210SEHJ01
-* College Tuition/Fees: CUURA210SEEB01
-* Computers: CUURA210SEEE01
-* Gas: CUURA210SS47014
-* Rent of Shelter: CUURA210SAS2RS
-* Coffee: CUURA210SEFP01
-* Laundry Services: CUURA210SEGD03
+#### New England Population: 14 Million
+#### Middle Atlantic Population: 60 Million
+#### South Atlantic Population: 62 Million
 
-Baltimore/Washington
-* College Textbooks: CUURA311SSEA011
-* Bedroom Furniture: CUURA311SEHJ01
-* College Tuition/Fees: CUURA311SEEB01
-* Computers: CUURA311SEEE01
-* Gas: CUURA311SS47014
-* Rent of Shelter: CUURA311SAS2RS
-* Coffee: CUURA311SEFP01
-* Laundry Services: CUURA311SEGD03
-
-Since each are the same size class according to the BLS, each of their weights are 1/3. 
+(14 + 60 + 62) = 136
+New England Weight = .1
+Middle Atlantic Weight = .44
+South Atlantic Weigt = .46
