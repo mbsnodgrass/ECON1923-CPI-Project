ce4152b2c8844dd3225752809c1a080a29e054b5
Maxwell Snodgrass <72468812+mbsnodgrass@users.noreply.github.com>
2022-10-11 19:25:48 -0400

    CompletePart1

--- CPIPart1.py
+++ CPIPart1.py
@@ -1,28 +1,53 @@
-New England (Location 1: 
+New England (Location 1: .10)
 * P1 Food: CUUR0110SAF1
 * P2 Gas: CUUR0110SS47014
-* P3 Rent of Shelter: CUUR0110SAS2RS
+* P3 Fuels/Utilities: CUUR0110SAH
 * P4 Apparrel: CUUR0110SAA
 
-Middle Atlantic (Location 2:
+Middle Atlantic (Location 2: .44)
 * P1 Food: CUUR0120SAF1
 * P2 Gas: CUUR0120SS47014
-* P3 Rent of Shelter: CUUR0120SAS2RS
-* P5 Educational Books/Supplies: CUUR0120SEEA
+* P5 Nonalcaholic Beverages: CUUR0120SAF114
 * P6 Food Away From Home: CUUR0120SEFV
 
-South Alantic (Location 3: 
+South Alantic (Location 3: .46)
 * P1 Food: CUUR0350SAF1
 * P2 Gas: CUUR0350SS47014
-* P3 Rent of Shelter: CUUR0350SAS2RS
-* P7 Personal Care Services: CUUR0350SEGC
-* P8 Transportation Services: CUUR0350SAS4
+* P7 Energy Services: CUUR0350SEHF
+* P8 Recreation: CUUR0350SAR
+
+## Location Weights
 
 #### New England Population: 14 Million
 #### Middle Atlantic Population: 60 Million
 #### South Atlantic Population: 62 Million
 
-(14 + 60 + 62) = 136
-New England Weight = .1
-Middle Atlantic Weight = .44
-South Atlantic Weigt = .46
+
+* New England Weight = .10
+* Middle Atlantic Weight = .44
+* South Atlantic Weight = .46
+
+## Item Weights
+
+New England
+* Food = .50
+* Gas = .25
+* Fuels/Utilities = .15
+* Apparrel = .10
+
+Middle Atlantic
+* Food = .45
+* Gas = .35
+* Nonalcaholic Beverages = .05
+* Food Away From Home = .15
+
+South Atlantic
+* Food = .40
+* Gas = .30
+* Energy Services = .20
+* Recreation = .10
+
+## Total Weights
+
+* Formula = .432(Food) + .317(Gas) + .015(Fuels/Utilities) + .01(Apparrel) + .022(Nonalcaholic Beverages) + .066(Food Away From Home) + .092(Energy Services) + .046(Recreation)
+             
